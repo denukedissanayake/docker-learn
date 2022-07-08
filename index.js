893b4f9ef56b22068e9c44afcd0a341eb52e0883
Denuke <ddnadhananja@gmail.com>
2022-07-09 04:04:09 +0530

    Initialize docker project

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app = express();
+const PORT = 3005;
+
+app.get('/', (req, res) => {
+    res.json("Docker Index").status(200);
+})
+
+
+app.listen(PORT, () => console.log(`Listning to PORT ${PORT}`))
